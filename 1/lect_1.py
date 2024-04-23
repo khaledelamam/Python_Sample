3dfe0d2b9a295f44de9028398ec4eab7332c4a08
khaledmohamed <167872443+khaledmohamed24@users.noreply.github.com>
2024-04-23 19:52:48 +0200

    Add files via upload

---
+++ 1/lect_1.py
@@ -0,0 +1,103 @@
+# hello this is my first program ever!
+# print("Hello itians!")
+
+# Variables
+# x = 5
+# y = 2
+# z = x + y
+# print(z)
+
+# greeting = "hi"
+# name = "yahia"
+# space = " "
+# sentence = greeting + space + name
+# print(sentence)
+
+# x = "5"
+# y = "2"
+# z = x + y
+# x = 5
+# y = 2
+# z = x + y
+# print(z)
+# x = 3.5
+# y = 3
+# print(x+y)
+
+# if else , indentations
+# x = "5"
+# if x == "5":
+#     print("x equals 5!")
+# else:
+#     print("x does NOT equal 5!")
+
+# f string
+# name = "yahia"
+# print("my name is "+name)
+# print(f"my name is {name}")
+
+# boolean datatype
+# x = 5
+# print(x == 5)
+# print(x > 5)
+# print(x < 5)
+# print(x <= 5)
+
+# logical gates
+# x = True
+# y = False
+# z = x and y
+# print(z)
+# q = x or y
+# print(q)
+# e = not x
+# print(e)
+# if x and y:
+#     print('hello world!')
+# x = (3==5)
+# y = (5==5)
+# z = x and y
+# print(z)
+
+# x = "5"
+# print(x)
+# print(type(x))
+# y = int(x)
+# z = y + 3
+# print(y)
+# print(type(y))
+# print(z)
+# x = "123asd"
+# y = int(x)
+# print(y)
+
+# x = 'yahia'
+# y = len(x)
+# print(y)
+# z = x[0:5]
+# print(z)
+# z = x.count('a')
+# print(z)
+# t = x.replace('a','l')
+# print(t)
+# t = x.isdigit()
+# print(t)
+
+# l = [1,2,True,4.5,"yahia"]
+# print(l)
+# x = l[4] # yahia
+# y = l[0] # 1
+# z = x + y # yahia + 1
+# print(z)
+# l = [5,1,2,3,4]
+# l.sort()
+# l.reverse()
+# print(l)
+# for element in l:
+#     x = element + 1
+#     print(x)
+
+# LOOPS
+# for i in range(5):
+#     print(i)
+# print('yahia')
